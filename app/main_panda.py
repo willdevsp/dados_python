53f999015cc776f1c7e0c95c3f5dd1daf3aa3229
Will <estudoswill@gmail.com>
2024-04-27 17:39:04 -0300

    gerando arquivo parquet

--- app/main_panda.py
+++ app/main_panda.py
@@ -1,8 +1,11 @@
 import uuid
+import pyarrow as pa
+import pyarrow.parquet as pq
 
 from sqlalchemy import create_engine
 import codecs
 import pandas as pd
+
 try:
     mydb = create_engine('mysql+mysqlconnector://root:password@127.0.0.1:3306/nome_banco', echo=True)
 
@@ -10,13 +13,20 @@ try:
 
     #mydb = connection.connect(host="localhost", database = 'nome_banco',user="root", passwd="password",use_pure=True)
     query = "Select * from pessoa;"
-    result_dataFrame = pd.read_sql(query,mydb)
+    result_dataFrame = pd.read_sql(query, mydb)
     df = pd.DataFrame(result_dataFrame)
 
     print(df)
 
-    df["uuid_convertido"] = list(map(lambda x: uuid.UUID(int=int.from_bytes(x)), df["id_binary"]))
+    df["id_binary"] = list(map(lambda x: str(uuid.UUID(int=int.from_bytes(x))), df["id_binary"]))
     print(df)
+
+    df.to_parquet('df.parquet',
+                  compression='gzip')
+
+    df_parquet = pd.read_parquet('df.parquet')
+
+    print(df_parquet)
 
     #mydb.close()
 except Exception as e:
